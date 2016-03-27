0add48540c1bcdb5e5c6b05f463f6371ea5b6fb4
eliezerwohl <eliezerwohl@gmail.com>
2016-03-27 18:40:00 -0400

    hk

--- server.js
+++ server.js
@@ -2,8 +2,21 @@ var express = require('express');
 var app = express();
 var logger = require('morgan');
 var mongoose = require('mongoose');
-var db = 'mongodb://localhost/projects';
-mongoose.connect(db);
+
+
+var uristring = process.env.MONGOLAB_URI || 
+  process.env.MONGOHQ_URL || 
+  'mongodb://localhost/projects';
+
+mongoose.connect(uristring, function (err, res) {
+  if (err) { 
+    console.log ('ERROR connecting to: ' + uristring + '. ' + err);
+  } else {
+    console.log ('Succeeded connected to: ' + uristring);
+  }
+});
+
+
 
 var Schema = mongoose.Schema;
 

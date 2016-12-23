873e6125a79179072b6dc5a5dae18dc21d80dfaa
Eliezer Wohl <eliezerwohl@gmail.com>
2016-12-23 13:58:15 -0500

    added google analytics

---
+++ public/javascript/google.js
@@ -0,0 +1,14 @@
+// angular.module('routerApp').controller('google', function($rootScope, $location, $window) {
+//    console.log("we got the google controller")
+//    // run()
+//    // function run($rootScope, $location, $window) {
+//    //      // initialise google analytics
+//  		// 	 $window.ga('create', 'UA-89424221-1', 'auto');
+//    //      // track pageview on state change
+//    //      $rootScope.$on('$stateChangeSuccess', function (event) {
+//    //      	console.log("switchs")
+//    //          $window.ga('send', 'pageview', $location.path());
+//    //      });
+//    //  }
+});
+
